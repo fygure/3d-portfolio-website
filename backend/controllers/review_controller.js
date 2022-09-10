8e8894503382372b9d5e1930ab93990d863fcf56
fygure <92011471+fygure@users.noreply.github.com>
2022-09-10 04:38:43 -0500

    finished backend

--- backend/controllers/review_controller.js
+++ backend/controllers/review_controller.js
@@ -1,2 +1,107 @@
 //This file keeps the reviews_routes.js file less bloated and clean
 //Need to import model because we need it to interact with database
+const Review = require('../models/review_model')
+const mongoose = require('mongoose')
+
+//Get all reviews (GET)
+const getReviews = async (req, res) => {
+    // {} finds all of them, ex {score: 15} would return all reviews with that score
+    // sorts in descending order (newest at top)
+    const reviews = await Review.find({}).sort({createdAt: -1}) 
+
+    res.status(200).json(reviews)
+}
+
+//Get single review by id (GET)
+const getReview = async (req, res) => {
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json( {error: 'No such review'} )
+    }
+
+
+    const review = await Review.findById(id)
+
+    //catches if not found
+    if(!review) {
+        return res.status(404).json({error: "No such review"})
+    }
+
+    //if success
+    res.status(200).json(review)
+}
+
+//Create new review (POST)
+const postReview = async (req, res) => {
+    //define body from review_model.js (title and score)
+    const {title, score} = req.body
+
+    // add doc to db
+    try {
+        //attempts to add new review
+        const review = await Review.create( {title, score} )
+        res.status(200).json(review) //sends that everything works and the json object back
+        
+    } catch(error) {
+        res.status(400).json( {error: error.message} )
+
+    }
+}
+
+//Delete a review (DELETE)
+const deleteReview = async (req, res) => {
+    //grab id route
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json( {error: 'No such review'} )
+    }
+
+    const review = await Review.findOneAndDelete( {_id: id} ) //_id for mongodb
+
+    //check if there is review to delete
+    if (!review) {
+        return res.status(400).json( {error: 'No such review'} )
+    }
+
+    //success
+    res.status(200).json(review)
+}
+
+//Update a review (PATCH/PUT)
+const updateReview = async (req, res) => {
+
+    //gets id
+    const { id } = req.params
+
+    //checks id valid
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json( {error: 'No such review'} )
+    }
+
+    const review = await Review.findOneAndUpdate( {_id: id}, {
+        //updates here
+        ...req.body
+    } )
+
+
+    if (!review) {
+        return res.status(400).json( {error: 'No such review'} )
+    }
+
+    //success
+    res.status(200).json(review)
+
+}
+
+
+
+//Exports functions
+module.exports = {
+    getReview,
+    getReviews,
+    postReview,
+    deleteReview,
+    updateReview
+}
--- backend/routes/reviews_routes.js
+++ backend/routes/reviews_routes.js
@@ -1,50 +1,28 @@
 const express = require('express')
-const Review = require('../models/review_model')
+const { postReview,
+        getReview,
+        getReviews,
+        deleteReview,
+        updateReview
+    } = require('../controllers/review_controller')
 
 //Router because express app is in server.js
 const router = express.Router()
 
 //GET all reviews ('/' = starting from route at line 97 of server.js: '/api/reviews/')
-router.get('/', (req, res) => {
-    res.json( {mssg: `GET all reviews`} )
-})
+router.get('/', getReviews)
 
 //GET single review
-router.get('/:id', (req, res) => {
-    res.json( {mssg: `GET a single review`} )
-})
-
+router.get('/:id', getReview)
 
 //POST a new review
-//added 'async' because Review.create() is an async function . . add 'await' before it
-//BLOATED FUNCTION => PUT OTHERS INTO A CONTROLLER FOLDER in backend
-router.post('/', async (req, res) => {
-    //req.body (data to send to server)
-    //define body from review_model.js (title and score)
-    const {title, score} = req.body
-
-    try {
-        //attempts to add new review
-        const review = await Review.create( {title, score} )
-        res.status(200).json(review) //sends that everything works and the json object back
-        
-    } catch(error) {
-        res.status(400).json( {error: error.message} )
-
-    }
-})
+//BLOATED FUNCTION => MOVED INTO A CONTROLLERS FOLDER in backend
+router.post('/', postReview) //calls the defined function
 
 //DELETE a review
-router.delete('/:id', (req, res) => {
-    res.json( {mssg: `DELETE a review`} )
-})
+router.delete('/:id', deleteReview)
 
 //UPDATE a review
-router.patch('/:id', (req, res) => {
-    res.json( {mssg: `UPDATE a review`} )
-})
-
-
-
+router.patch('/:id', updateReview)
 
 module.exports = router
